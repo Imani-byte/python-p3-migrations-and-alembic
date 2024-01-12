d6fce3338e894654979e17d1d63ca5d04cf07ad0
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2024-01-12 19:10:57 +0300

    completed assignment

---
+++ migrations/versions/08f64d9098ea_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 08f64d9098ea
+Revises: 
+Create Date: 2024-01-12 18:56:56.132572
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '08f64d9098ea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
